af13d4bf18eb259eddc5533a8f0cb92d25b8d853
AlanL543 <73874120+AlanL543@users.noreply.github.com>
2021-10-31 19:36:51 +0100

    Add files via upload

--- src/main/java/edu/supavenir/spanimals/controller/CaracteristiqueController.java
+++ src/main/java/edu/supavenir/spanimals/controller/CaracteristiqueController.java
@@ -1,5 +1,28 @@
 package edu.supavenir.spanimals.controller;
 
-public class CaracteristiqueController {
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tomcat.util.http.parser.Cookie;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CaracteristiqueController {
+	@GetMapping("/set")
+	public String setCookie(HttpServletResponse response) {
+	    // set a new cookie
+	    Cookie cookie = new Cookie(id, libelle,);
+	    // add cookie in server response
+	    response.addCookie(cookie);
+
+	    return "Spring Boot Cookies";
+	}
+public void TestAnimalSimilaire(int caracteristique)  {
+	switch(caracteristique) {
+	
+	case 1:
+		"espece=={{espece}}";
+		break;
+	}
+}
 }
